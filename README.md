b9e76d57fc063b06d6f535c249d665363ffdf213
Andor233 <16273755+Andor233@users.noreply.github.com>
2021-01-22 14:44:45 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RosUnreal
+
+Developed with Unreal Engine 4
---
+++ Source/RosUnreal/RosUnrealGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RosUnrealGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROSUNREAL_API ARosUnrealGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
